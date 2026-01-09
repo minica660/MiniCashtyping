4fe22bc0e0930b0ae887a1649931eae336818e6c
MiniCash <tminecraft2143@gmail.com>
2026-01-09 22:38:30 +0900

    Create Event.java

---
+++ src/main/java/MiniCash/miniCashtypinggame/Event.java
@@ -0,0 +1,39 @@
+package MiniCash.miniCashtypinggame;
+
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.UUID;
+
+import static MiniCash.miniCashtypinggame.MPublic.*;
+
+public class Event implements Listener {
+
+    @EventHandler
+    public void Chat(AsyncChatEvent event) {
+
+        if (game){
+            event.setCancelled(true);
+            if (chatcheck.containsKey(event.getPlayer().getUniqueId())){
+               event.getPlayer().sendMessage("§cすでに正解しています\nオープンチャットはしばらくお待ちください");
+            } else{
+                String message = PlainTextComponentSerializer.plainText().serialize(event.message());
+                if (list.contains(message)){
+                    UUID id = event.getPlayer().getUniqueId();
+                    chatcheck.put(id,true);
+                    event.getPlayer().sendMessage("§b§l正解！！");
+                }
+            }
+
+
+        }
+
+    }
+
+}
+
+
+
+
